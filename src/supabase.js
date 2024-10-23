3d688999be1651c49cb55ee9004489234f314f02
mxxt4pf <mxxt.net.4pf@gmail.com>
2024-10-23 14:56:43 -0600

    changed supabase db api key values for fetching real time data

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lfkfmypyankkrtcylleo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxma2ZteXB5YW5ra3J0Y3lsbGVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk2NTA0MzAsImV4cCI6MTk4NTIyNjQzMH0.bo4nfBxzfsAIcqahzmAqtzHOqsrmY5cdBv684eOY5FI";
+const supabaseUrl = "https://ucykchstsicryrljdmhw.supabase.co";
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjeWtjaHN0c2ljcnlybGpkbWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3MTQ3NDcsImV4cCI6MjA0NTI5MDc0N30.fkbkjQZTZayyVaoJ59eIVAM2g0vQAb5YnS7S7AZ48kQ'
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
